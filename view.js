09f0ac2a21019f6c8d8d2fdc3c14052d0eb5665d
Leon Parsons <MY_NAME@example.com>
2023-02-04 17:22:42 +1300

    Syntax change for previous commit

--- view.js
+++ view.js
@@ -7,7 +7,9 @@ let dropDowns = [raceDropDown,classDropDown];
 export function fillRaceDropDown(obj) {
     const keys = Object.keys(obj);
     const objArrays = Object.entries(obj);
+
     for( let i = 0; i < keys.length; i++) {
+        
         for (let j = 0; j < objArrays[i][1].length; j ++) {
             let newOption = new Option(objArrays[i][1][j]);
             dropDowns[i].add(newOption);
