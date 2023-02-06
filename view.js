8881db52f4a7e9690732d39eca53dabbea72772e
Leon Parsons <MY_NAME@example.com>
2023-02-06 18:42:42 +1300

    Updates View more efficiently retrieve elements

--- view.js
+++ view.js
@@ -1,9 +1,6 @@
-const classDropDown = document.getElementById('class-dd');
-const raceDropDown = document.getElementById('race-dd');
-const alignmentOptions = document.getElementById('alignment-dd');
-const backgroundOptions = document.getElementById('background-dd');
-
-let dropDowns = [raceDropDown, classDropDown, alignmentOptions, backgroundOptions];
+//Select (dropdown elements) retrieved by class
+const optionDropdowns = document.getElementsByClassName('option-dd');
+const statDropDowns = document.getElementsByClassName('stat-dd');
 
 // Loops through each array in object by key, adding dropdown options per key
 export function fillOptionsDropDown(obj) {
@@ -15,15 +12,13 @@ export function fillOptionsDropDown(obj) {
 
         for (let j = 0; j < objArrays[i][1].length; j ++) {
             let newOption = new Option(objArrays[i][1][j]);
-            dropDowns[i].add(newOption);
+            optionDropdowns[i].add(newOption);
         }
     }
 };
 
 //Fill stats drop downs
 export function fillStatsDropDown(obj) {
-     const statDropDowns = document.getElementsByClassName('stat-dd');
-
     const keys = Object.keys(obj);
     const objArrays = Object.entries(obj);
     console.log(objArrays[0][1][0]);
