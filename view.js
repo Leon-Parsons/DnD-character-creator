745156f9341d2b9981e240541f3d2eab894dad11
Leon Parsons <MY_NAME@example.com>
2023-02-08 13:39:12 +1300

    Makes stat drop downs have different initial vals

--- view.js
+++ view.js
@@ -1,6 +1,7 @@
 //Select (dropdown elements) retrieved by class
-const optionDropdowns = document.getElementsByClassName('option-dd');
+const optionDropDowns = document.getElementsByClassName('option-dd');
 const statDropDowns = document.getElementsByClassName('stat-dd');
+
 
 // Loops through each array in object by key, adding dropdown options per key
 export function fillOptionsDropDown(obj) {
@@ -9,25 +10,30 @@ export function fillOptionsDropDown(obj) {
     const objArrays = Object.entries(obj);
 
     for( let i = 0; i < keys.length; i++) {
-
-        for (let j = 0; j < objArrays[i][1].length; j ++) {
+        for (let j = 0; j < objArrays[i][1].length; j++) {
             let newOption = new Option(objArrays[i][1][j]);
-            optionDropdowns[i].add(newOption);
+            optionDropDowns[i].add(newOption);
         }
     }
+    optionDropDowns[2].options[2].disabled = true;
 };
 
 //Fill stats drop downs
 export function fillStatsDropDown(obj) {
+
     const keys = Object.keys(obj);
     const objArrays = Object.entries(obj);
-    console.log(objArrays[0][1][0]);
 
-    for (let i = 0; i < 7; i++ ) {
-    
-        for (let j = 0; j < 7; j++) {
+    for (let i = 0; i < 6; i++ ) {
+        for (let j = 0; j < 6; j++) {
             let newStatOption = new Option(objArrays[0][1][j]);
             statDropDowns[i].add(newStatOption);
+            statDropDowns[j].value = objArrays[0][1][j];
         }
     }
+
+console.log("hi");
+
 };
+
+
